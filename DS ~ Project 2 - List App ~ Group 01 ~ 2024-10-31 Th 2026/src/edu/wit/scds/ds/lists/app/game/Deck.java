8e06f1b9aeb8b19bc97223fc9bc4326e7de3bb93
antho <anthonyferrucci05@gmail.com>
2024-11-21 18:12:37 -0500

    please work

--- DS ~ Project 2 - List App ~ Group 01 ~ 2024-10-31 Th 2026/src/edu/wit/scds/ds/lists/app/game/Deck.java
+++ DS ~ Project 2 - List App ~ Group 01 ~ 2024-10-31 Th 2026/src/edu/wit/scds/ds/lists/app/game/Deck.java
@@ -28,25 +28,99 @@ package edu.wit.scds.ds.lists.app.game;
 /**
  * Representation of a deck of cards
  *
- * @author Your Name // TODO
+ * @author //TODO
  *
  * @version 1.0 2024-10-31 Initial implementation
  */
 public class Deck extends Pile
-	{
-	// TODO implement this
+    {
+    
+    /**
+     * Constructor
+     * 
+     */
+    public Deck()
+    {
+    
+    final Suit[] suits = Suit.values() ;
+    final Rank[] ranks = Rank.values() ;
+    
+    for ( final Suit suit : suits )
+        {
+
+        // skip placeholder suit
+        if ( Suit.NONE.equals( suit ) )
+            {
+            continue ;
+            }
+
+        for ( final Rank rank : ranks )
+            {
+
+            // skip non-playing card(s) - Joker
+            if ( Rank.JOKER.equals( rank ) )
+                {
+                continue ;
+                }
+
+            // build a card
+            final Card newCard = new Card( suit, rank ) ;
+            System.out.printf( " %s", newCard ) ;
+
+            // keep track of it
+            this.cards.add( newCard ) ;
+            }
+
+        }
+    
+    this.shuffle();
+    
+    }
+    
+    
+    
+    /**
+     * Deal ONE card
+     * 
+     * @return card
+     */
+    public Card deal()
+    {
+    
+    return this.remove();
+    
+    }
 
 
-	/**
+    /**
      * (optional) test driver
      * 
      * @param args
      *     -unused-
      */
-	public static void main( String[] args )
-		{
+    public static void main( String[] args )
+        {
         // OPTIONAL for testing and debugging
+        
+        Deck myDeck = new Deck();
+        
+        //these are all hidden
+        System.out.println( myDeck.toString() );
+        
+        //flip them over
+        
+        for (Card card : myDeck.cards)
+            {
+            card.flip();
+            }
+        
+        System.out.println( myDeck.toString() ) ;
+            
+        System.out.println(myDeck.deal());
+        
+        System.out.println( myDeck ) ;
+        
 
-		}	// end main()
+        }   // end main()
 
-	}	// end class Deck
+    }   // end class Deck
